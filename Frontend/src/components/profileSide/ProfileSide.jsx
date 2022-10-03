2770096184ce4f0d57bf2f68ca1c6470641e572b
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-10-03 18:09:07 -0300

    ProfileCard

--- Frontend/src/components/profileSide/ProfileSide.jsx
+++ Frontend/src/components/profileSide/ProfileSide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LogoSearch from '../logoSearch/LogoSearch'
+import ProfileCard from '../profileCard/ProfileCard'
 import styles from '../profileSide/ProfileSide.module.css'
 
 const ProfileSide = () => {
     return (
         <div className={styles.profileSide}>
+            <ProfileCard />
             <LogoSearch />
         </div>
     )
