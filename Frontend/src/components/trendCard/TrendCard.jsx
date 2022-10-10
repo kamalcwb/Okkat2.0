c6e943f298aa07e57b4b785d88058c749b55ee21
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-10-10 18:36:12 -0300

    TrendCard
    Create TrendCard

---
+++ Frontend/src/components/trendCard/TrendCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './TrendCard.module.css'
+
+const TrendCard = () => {
+    return (
+        <div className={styles.trendCard}>
+
+        </div>
+    )
+}
+
+export default TrendCard
