0b4636ac6934764c50de14e8cbfa9b93e49a3964
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-10-15 11:19:27 -0300

    Form structuring

--- Frontend/src/pages/Register/Register.jsx
+++ Frontend/src/pages/Register/Register.jsx
@@ -10,7 +10,31 @@ const Register = () => {
                     <h6>Conecte-se com o mundo</h6>
                 </div>
             </div>
-            <h1>Form</h1>
+            <SignUp />
+        </div>
+    )
+}
+
+function SignUp() {
+    return (
+        <div className={styles.right}>
+            <form action='' className={styles.infoForm}>
+                <h3>Inscrever-se</h3>
+                <div>
+                    <input type="text" className={styles.infoInput} name="name" placeholder='seu nome' />
+                    <input type="text" className={styles.infoInput} name="surname" placeholder='seu sobrenome' />
+                </div>
+                <div>
+                    <input type="text" className={styles.infoInput} name="username" placeholder='seu nome de usuário' />
+                </div>
+                <div>
+                    <input type="text" className={styles.infoInput} name="email" placeholder='seu email' />
+                </div>
+                <div>
+                    <input type="password" className={styles.infoInput} name="password" placeholder='sua senha' />
+                    <input type="password" className={styles.infoInput} name="confirmPassword" placeholder='confirme sua senha' />
+                </div>
+            </form>
         </div>
     )
 }
