8dad7555598186b8b9eb42d9444e3925732ac42a
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-10-18 14:27:57 -0300

    ShareModal

--- Frontend/src/components/rightSide/RightSide.jsx
+++ Frontend/src/components/rightSide/RightSide.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 import styles from './RightSide.module.css'
 import Home from '../../img/home.png'
 import Noti from '../../img/noti.png'
 import Comment from '../../img/comment.png'
 import { UilSetting } from '@iconscout/react-unicons'
 import TrendCard from '../trendCard/TrendCard'
+import ShareModal from '../shareModal/ShareModal'
 
 const RightSide = () => {
+
+    const [modalOpen, setModalOpen] = useState(false)
+
     return (
         <div className={styles.rightSide}>
             <div className={styles.navIcons}>
@@ -16,9 +21,14 @@ const RightSide = () => {
                 <img src={Comment} alt='' />
             </div>
             <TrendCard />
-            <button className={styles.button}>Compartilhar</button>
-
-        </div>
+            <button className={styles.button} onClick={() => setModalOpen(true)}
+            >
+                Compartilhar
+            </button>
+            <ShareModal
+                modalOpen={modalOpen}
+                setModalOpen={setModalOpen} />
+        </div >
     )
 }
 
